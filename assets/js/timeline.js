0a10619b8dbf2be226ec74c280288ac7c9aaefea
Ashawe <harshsaglani@gmail.com>
2020-03-14 00:32:38 +0530

    :construction: Add work experience page :sparkles:

--- assets/js/timeline.js
+++ assets/js/timeline.js
@@ -1,27 +1,19 @@
-$( document ).ready(function() {
+$(document).ready(function() {
     var elem = $('#timeline').children().first();
-    var offset = $(elem).height()/2;
-    $(elem).css("margin-top",-offset);
-    $('#timeline_row').css("margin-top",offset);
-    $('#timeline_row').css("margin-bottom",offset);
+    var offset = $(elem).height() / 2;
+    $(elem).css("margin-top", -offset);
+    $(elem).parent().css("margin-top", offset);
+    $('#timeline').height($('#timeline').height() - $(elem).height() - 16);
+    $('.work-exp.aside').css("margin-top", $(elem).height() / 2);
 
-    var year = $('.timeline-item-right');
-    $(year).each(function(){
-        $(this).css("margin-top",offset-$(year).children().first().height()-8);        
+    var year = $('.year .my_big_tag');
+    var year_height = $(year).height();
+    $(year).each(function() {
+        $(this).css("margin-top", $(this).parent().parent().height() / 2 - year_height / 2 - 8);
     });
-
-    year = $('.timeline-item-year');
-    $(year).each(function(){
-        $(this).css("margin-top",offset-$(year).children().first().height()-8);
-    });
-    
 
     var badge = $('.timeline-badge');
-    $(badge).each(function(){
-        $(this).css("margin-top",offset-$(year).children().first().height()+16);
+    $(badge).each(function() {
+        $(this).css("margin-top", $(this).parent().height() / 2);
     });
-
-    var h = $('#timeline').height();
-    $('#timeline').height( h - offset - 16);
-
 });
