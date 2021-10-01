98a1a09c926326f75a0138efaee8046577c7c616
kunal kumar <kunalkumar06022003@gmail.com>
2021-10-01 10:34:12 +0530

    fix: added dark mode in my_story.html

--- assets/js/dark.js
+++ assets/js/dark.js
@@ -1,12 +1,14 @@
 function darkToggle() {
   let element = document.body;
-  let headernav = document.getElementById("navbar");
-  let h3 = document.getElementsByTagName("h3");
+  let headernav = document.querySelector(".nav-inner");
+  let h3 = document.querySelectorAll("h3");
+  let socialLinks = document.querySelectorAll(".social-icons a");
 
   element.classList.toggle("dark");
   headernav.classList.toggle("nav-dark");
 
-  for (let i = 0; i < h3.length; i++) {
-   h3[i].classList.toggle("sec-dark");
-  }
+  for (let i = 0; i < h3.length; i++) h3[i].classList.toggle("sec-dark");
+
+  for (let i = 0; i < socialLinks.length; i++)
+    socialLinks[i].classList.toggle("sec-dark");
 }
