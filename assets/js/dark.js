66f26833dfab80dea85fec95017090b956690f41
kunal kumar <kunalkumar06022003@gmail.com>
2021-10-01 10:40:28 +0530

    feat: added dark mode in projects.html

--- assets/js/dark.js
+++ assets/js/dark.js
@@ -2,13 +2,14 @@ function darkToggle() {
   let element = document.body;
   let headernav = document.querySelector(".nav-inner");
   let h3 = document.querySelectorAll("h3");
+  let h4 = document.querySelectorAll("h4");
   let socialLinks = document.querySelectorAll(".social-icons a");
 
   element.classList.toggle("dark");
   headernav.classList.toggle("nav-dark");
 
   for (let i = 0; i < h3.length; i++) h3[i].classList.toggle("sec-dark");
-
+  for (let i = 0; i < h4.length; i++) h4[i].classList.toggle("sec-dark");
   for (let i = 0; i < socialLinks.length; i++)
     socialLinks[i].classList.toggle("sec-dark");
 }
